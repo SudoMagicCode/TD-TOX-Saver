c2375b99a028cb97746fc39fbe3a89e6e123d75a
Matthew Ragan <raganmd@gmail.com>
2025-03-16 03:34:05 -0700

    Automation updates

--- scripts/build_tox.py
+++ scripts/build_tox.py
@@ -2,56 +2,60 @@ import subprocess
 import os
 import shutil
 
-import gitVersion
-import build_settings
-import env_var_utils
-import read_td_log
+
+import td_builder.build_settings
+import td_builder.env_var_utils
+import td_builder.gitVersion
+import td_builder.read_td_log
 
 artifact_dir_name = "artifacts"
 targets_dir_name = "targets"
 dist_info_name = "dist_info.json"
+build_settings_file = "buildSettings.json"
 
 dist_info: dict = {}
-project_file = build_settings.project_file
 
 
 def main():
-    print('> verifying output directories are created...')
-    # Verify dist directory exists
-    dist_dir = build_settings.dest_dir+"/"
-    if not os.path.isdir(dist_dir):
-        print('> creating release...')
-        os.mkdir(dist_dir)
+    print('> creating release...')
 
-    latest_dir = build_settings.latest_dir
-    if not os.path.isdir(latest_dir):
-        print('> creating release/latest...')
-        os.mkdir(latest_dir)
+    build_settings = td_builder.build_settings.settings()
+    build_settings.load_from_json(build_settings_file)
+
+    # Verify dist directory exists
+    dist_dir = f"{build_settings.dest_dir}/"
+    print('> verifying output directories are created...')
+    if not os.path.isdir(dist_dir):
+        print('-> creating directories...')
+        os.makedirs(dist_dir, exist_ok=True)
 
     print("> Starting deploy process...")
 
     print("-> Finding Version Info...")
-    dist_info = gitVersion.GetVersioningInfo()
+    dist_info = td_builder.gitVersion.distInfo()
 
-    print(f"--> Creating build {dist_info.get('major_minor')}")
+    print(
+        f"--> Creating build {dist_info.major}.{dist_info.minor}.{dist_info.patch}")
 
     # set up env vars
-    env_var_utils.set_env_vars(
-        build_settings=build_settings, dist_info=dist_info)
+    td_builder.env_var_utils.set_env_vars(
+        build_settings=build_settings.env_vars, dist_info=dist_info.asDict)
 
     # run td project
     print("--> Starting TouchDesigner")
     td_version = f"C:/Program Files/Derivative/TouchDesigner.{build_settings.td_version}/bin/TouchDesigner.exe"
-    subprocess.call([td_version, project_file])
+    subprocess.call([td_version, build_settings.project_file])
 
-    read_td_log.write_log_to_cloud(build_settings.log_file)
+    td_builder.read_td_log.write_log_to_cloud(
+        build_settings.log_file)
 
     print("--> Zipping package")
     shutil.make_archive(
-        f"{build_settings.dest_dir}/package", 'zip', build_settings.latest_dir)
+        build_settings.package_dir, 'zip', root_dir=build_settings.package_dir)
 
     # cleanup environment variable keys
-    env_var_utils.clear_env_vars(build_settings=build_settings)
+    td_builder.env_var_utils.clear_env_vars(
+        build_settings=build_settings.env_vars)
 
 
 if __name__ == "__main__":
